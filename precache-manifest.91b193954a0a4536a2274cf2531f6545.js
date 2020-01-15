7f7103522ca3b5d3c5a8b281a80d1374e1ec0169
Luis Nesi <lenesi@hotmail.com>
2020-01-15 12:39:13 +0000

    Updates

---
+++ precache-manifest.91b193954a0a4536a2274cf2531f6545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e9df919a314fbde1d6ac6b94a1544d",
+    "url": "/publicis-test/index.html"
+  },
+  {
+    "revision": "9e3b5f85d26aa2b568b2",
+    "url": "/publicis-test/static/css/main.3b4ebc4a.chunk.css"
+  },
+  {
+    "revision": "a98d57d7f8bbadf8be35",
+    "url": "/publicis-test/static/js/2.345e4392.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/publicis-test/static/js/2.345e4392.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e3b5f85d26aa2b568b2",
+    "url": "/publicis-test/static/js/main.09635abc.chunk.js"
+  },
+  {
+    "revision": "b2ef87f041b4c2888510",
+    "url": "/publicis-test/static/js/runtime-main.437ea657.js"
+  },
+  {
+    "revision": "afa9a0f1e911b52b3bc3a7e618507a35",
+    "url": "/publicis-test/static/media/arrow.afa9a0f1.svg"
+  }
+]);
